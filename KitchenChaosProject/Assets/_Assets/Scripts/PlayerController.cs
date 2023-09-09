8d9fc49ba50bdc8ed7cab68eb63d8b794e9a7ab0
Peter <petergreen880@yahoo.com>
2023-09-08 20:38:38 -0400

    Refactored Game Movement Input

--- KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
+++ KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
@@ -9,29 +9,12 @@ public class PlayerController : MonoBehaviour
 
     private bool isWalking;
 
+    [SerializeField] private GameInput gameInput;
+
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = 1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = 1;
-        }
-
-        inputVector = inputVector.normalized;
-
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
         float rotateSpeed = 10f;
