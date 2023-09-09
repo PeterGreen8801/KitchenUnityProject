7101c4af7e3d22c69c73aafef1fbcdbb23c101eb
Peter <petergreen880@yahoo.com>
2023-09-08 20:08:53 -0400

    Added Animation via PlayerAnimator script

--- KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
+++ KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -35,7 +37,14 @@ public class PlayerController : MonoBehaviour
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
 
+        isWalking = moveDirection != Vector3.zero;
+
         transform.position += moveSpeed * Time.deltaTime * moveDirection;
 
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }
