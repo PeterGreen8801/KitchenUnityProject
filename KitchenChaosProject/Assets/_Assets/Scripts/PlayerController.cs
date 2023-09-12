c618ed16acbe08a00a031e189c1fe7cc0553e738
Peter <petergreen880@yahoo.com>
2023-09-11 23:41:01 -0400

    Update PlayerController.cs

--- KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
+++ KitchenChaosProject/Assets/_Assets/Scripts/PlayerController.cs
@@ -17,12 +17,21 @@ public class PlayerController : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y);
 
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = 0.7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if (canMove)
+        {
+            transform.position += moveDistance * moveDirection;
+        }
 
         isWalking = moveDirection != Vector3.zero;
 
-        transform.position += moveSpeed * Time.deltaTime * moveDirection;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
 
     }
 
