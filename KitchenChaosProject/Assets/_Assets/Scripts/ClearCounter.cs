73a2d99d574761bc7a278433b303640ce68ab03d
Peter <petergreen880@yahoo.com>
2023-11-18 18:29:33 -0500

    Update ClearCounter.cs

--- KitchenChaosProject/Assets/_Assets/Scripts/ClearCounter.cs
+++ KitchenChaosProject/Assets/_Assets/Scripts/ClearCounter.cs
@@ -10,7 +10,28 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(PlayerController player)
     {
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
 
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 
 
